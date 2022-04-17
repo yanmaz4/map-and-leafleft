34261278ca3aa004330227f2c6f749404d12b321
yanmaz4 <yanmaerc@gmail.com>
2022-04-17 15:30:39 +0200

    Map.js and Tabs.js are added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./styles.css";
-
+import Tabs from "./Tabs";
+import TabList from "react-tabs/lib/components/TabList";
+import TabPanel from "react-tabs/lib/components/TabPanel";
+import Tab from "react-tabs/lib/components/Tab";
+import MyMap from "./COMPONENTS/MyMap";
+import { LatLng } from "leaflet";
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +16,7 @@ export default function App() {
             Create a file <b>Tabs.js</b>
           </li>
           <li>
-            In this file, create tabs with{" "}
+            In this file, create tabs with{"Tabs"}
             <b>
               <a
                 rel="noopener noreferrer"
@@ -19,6 +24,11 @@ export default function App() {
                 href="https://github.com/reactjs/react-tabs"
               >
                 react-tabs
+                <Tabs />
+                <TabList />
+                <TabPanel />
+                <Tab />
+            
               </a>
             </b>
           </li>
@@ -26,25 +36,27 @@ export default function App() {
             Create a file <b>MyMap.js</b>
           </li>
           <li>
-            In this file create a map with{" "}
+            In this file create a map with{"MyMap"}
             <b>
               <a
                 rel="noopener noreferrer"
                 target="_blank"
                 href="https://react-leaflet.js.org/"
-              >
-                React Leafleft
-              </a>
+              ></a>
             </b>
           </li>
           <li>
-            You component MyMap should have two properties <b>lng</b> and{" "}
+            You component MyMap should have two properties <b>lng</b> and
+            {"lat "}
             <b>lat</b>
           </li>
           <li>Import these two components down below </li>
         </ul>
       </div>
-      <div className="block">Import your components here</div>
+      <div className="block">
+        <Tabs />
+        <MyMap/>
+      </div>
     </div>
   );
 }
